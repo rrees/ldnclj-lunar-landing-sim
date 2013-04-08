496797a6b87b969d0f585e35e58d7b769552996e
Daniel Barlow <dan@telent.net>
2013-04-08 20:42:40 +0100

    failing acceptance test

--- test/lunar_landing/core_test.clj
+++ test/lunar_landing/core_test.clj
@@ -8,5 +8,13 @@
 		(is (not (nil? (lander/adjust {}))))))
 
 (deftest land-at-rest-remains-at-rest
-  (testing "Lander moved from rest"
+  (testing "Do not thrust when lander is on the ground"
     (is (= 0 (:thrust (lander/adjust {:height 0 :velocity 0}))))))
+
+(def maximum-landing-velocity 1.0)
+
+(deftest lander-lands-from-200m
+  (testing "It does not break"
+    (let [state (lander/fall-from-height 200)]
+      (is (= 0 (:height state)))
+      (is (< maximum-landing-velocity (:velocity state))))))
